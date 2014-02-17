ac63d2d1825a9aeaa9a77dce8707371fea2f88b4
ana-balica <ana.balica@gmail.com>
2014-02-17 23:07:21 +0200

    download zips from ocw and add flash messages

--- lab1/proxy/main.py
+++ lab1/proxy/main.py
@@ -1,14 +1,15 @@
 import urllib
 from urlparse import urljoin
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, flash
 
 
 app = Flask(__name__)
-books = {"head_first_web_design": "http://it-ebooks.info/go.php?id=378-1392675790-f597a7940b48b95cb72f848efad42016",
-         "head_first_design_patterns": "http://it-ebooks.info/go.php?id=252-1392675832-83f522728f81e90f89ed476ff9c95e55",
-         "head_first_data_analysis": "http://it-ebooks.info/go.php?id=250-1392675874-42981d0d415bf9387170465f29d5116c",
-         "head_first_rails": "http://it-ebooks.info/go.php?id=374-1392678356-5810778fb9f821ec03ace650664cd30d",
-         "head_first_wordpress": "http://it-ebooks.info/go.php?id=379-1392678394-f9ed6d1e4fdefb737c271a08b7310af7"}
+app.secret_key = 'some_secret_key_whatever'
+books = {"WGS.115": "http://ocw.mit.edu/ans15436/ZipForEndUsers/WGS/wgs-115-spring-2013/wgs-115-spring-2013.zip",
+         "6.046J": "http://ocw.mit.edu/ans15436/ZipForEndUsers/6/6-046j-spring-2012/6-046j-spring-2012.zip",
+         "ES.S60": "http://ocw.mit.edu/ans15436/ZipForEndUsers/ES/es-s60-spring-2013/es-s60-spring-2013.zip",
+         "18.353J": "http://ocw.mit.edu/ans15436/ZipForEndUsers/18/18-353j-fall-2012/18-353j-fall-2012.zip",
+         "24.118": "http://ocw.mit.edu/ans15436/ZipForEndUsers/24/24-118-spring-2013/24-118-spring-2013.zip"}
 
 
 @app.route('/')
@@ -20,8 +21,9 @@ def main():
 def no_proxy():
     if request.method == 'POST':
         title = request.form["book"]
-        filepath = urljoin("downloads/", title + '.pdf')
+        filepath = urljoin("downloads/", title + '.zip')
         urllib.urlretrieve(books[title], filepath)
+        flash('Successfully downloaded {0}'.format(title))
     return render_template('proxy.html', page_title="No proxy")
 
 
